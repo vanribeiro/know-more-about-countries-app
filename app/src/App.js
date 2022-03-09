0926ba277f9e86e7bd57efc655b5f4cf2778299a
vanribeiro <vanribeiro.dev@gmail.com>
2022-03-08 21:30:59 -0300

    update

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,11 @@
 import GlobalStyle from "./components/GlobalStyle";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Details from "./pages/Deitails";
-import Home from "./pages/Home";
-import PageNotFound from "./pages/PageNotFound";
 import { lightModeTheme, darkModeTheme } from "./components/UI/themes";
 import { useState } from "react";
 import { ThemeProvider } from "styled-components";
+import Home from "./pages/Home";
+import Details from "./pages/Deitails";
+import PageNotFound from "./pages/PageNotFound";
 import Region from "./pages/Region";
 
 function App() {
@@ -23,13 +23,13 @@ function App() {
               path="/"
               exact
               element={<Home toggleTheme={toggleTheme} />}
-            />
+              />
             <Route path="country/:id" element={<Details toggleTheme={toggleTheme} />} />
             <Route path="region/:id" element={<Region toggleTheme={toggleTheme} />} />
             <Route
-              path="*"
-              element={<PageNotFound toggleTheme={toggleTheme} />}
-            />
+            path="*"
+            element={<PageNotFound toggleTheme={toggleTheme} />}
+          />
           </Routes>
         </Router>
       </ThemeProvider>
